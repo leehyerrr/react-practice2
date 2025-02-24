f227ed18b496e2de4174e0c5312df0aa021cf366
leehyerrr <leehyerrr@gmail.com>
2025-02-24 23:01:10 +0900

    prettier설정 변경

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: "none",
-  arrowParens: "avoid",
-  semi: false,
-  printWidth: 90,
-};
+  bracketSpacing: true, // { foo: bar } 대신 {foo: bar} 사용
+  jsxBracketSameLine: false, // JSX 태그 줄바꿈 시 닫는 괄호(>) 같은 줄에 배치
+  singleQuote: true, // 문자열에 작은따옴표 사용 (' 대신 ")
+  trailingComma: 'none', // 마지막 요소 뒤에 쉼표 X
+  arrowParens: 'avoid', // 화살표 함수에서 매개변수 1개일 때 괄호 생략
+  semi: false // 세미콜론 사용 X
+  // printWidth: 90, // 한 줄 최대 90자
+}
